525da8ea4a20909422f9a262863497639019da3f
Riley <dmurch1229@gmail.com>
2025-11-14 11:53:46 -0600

    functioning versiongit add .

--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <filesystem>
+
+std::string notesPath = "./notes/";
 
 void makeNote(std::string name, std::string contents) {
-  std::ofstream writeFile(name);
+  std::ofstream writeFile(notesPath + name);
   writeFile << contents;
   writeFile.close();
-  std::ifstream readFile(name);
+}
+std::string readNote(std::string name) {
+  std::ifstream readFile(notesPath + name);
   if (readFile.is_open()) {
-    std::string line;
-    while(std::getline(readFile,line)) {
-      std::cout << line;
-    }
-    readFile.close();
+    std::string note;
+    std::getline(readFile,note);
+    return note;
   }
-  
+  else return "error! could not open " + name;
 }
 
 
 
 int running = 1;
 int main(int argc, char **argv) {
+  std::filesystem::create_directory(notesPath);
   int i = 0;
   std::string args[argc];
   if(argc >= 1) {
@@ -35,27 +39,28 @@ int main(int argc, char **argv) {
   std::string input;
   int handled;
   while(running) {
-    handled = 0;
     std::cout << "> ";
     std::getline(std::cin,input);
-    if (input == "quit") { running = 0; handled = 1; }
-    if (!handled) {
-      std::stringstream ss(input);
-      std::string arg;
-      std::vector<std::string> args;
-      while (ss >> arg) {
-        args.push_back(arg);
-      }
-      int i = 1;
+    std::stringstream ss(input);
+    std::string arg;
+    std::vector<std::string> args;
+    while (ss >> arg) args.push_back(arg);
+    if (args[0] == "quit") { running = 0; }
+    else if (args[0] == "make") {
+      int i = 2;
       std::string cont;
       while (i < args.size()) {
         cont += args[i] + " ";
         i++;
       }
-      makeNote(args[0], cont);
+      makeNote(args[1], cont);
     }
-    //if (!handled) std::cout << input << std::endl;
-
+    else if (args[0] == "read") std::cout << readNote(args[1]) << std::endl;
+    else {
+      std::cout << "no command!" << std::endl;
+        for (auto& p : std::filesystem::directory_iterator(notesPath))
+        std::cout << p.path().filename().string() << "\n";
+    }
   }
   return 0;
 }
